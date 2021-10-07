7bf04586f0087841ec960552acdbae43721e2d2b
Richard Highness <richard@highness.org>
2021-10-06 18:38:20 -0600

    Updated script.sh

--- script/script.sh
+++ script/script.sh
@@ -2,5 +2,13 @@
 export DEBIAN_FRONTEND="noninteractive"
 sudo apt install -y software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
 sudo apt install -y ansible
+sudo apt install -y openjdk-11-jdk
+sudo apt install -y jenkins
+sudo systemctl start jenkins
+echo "The Jenkins initial Admin Password is:"
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
