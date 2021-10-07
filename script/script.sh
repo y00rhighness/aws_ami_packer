e0063390b4e1b623e6679adfa863946a647ab9a5
Richard Highness <richard@highness.org>
2021-10-07 10:42:20 -0600

    Updated script.sh with additional commands

--- script/script.sh
+++ script/script.sh
@@ -2,12 +2,14 @@
 export DEBIAN_FRONTEND="noninteractive"
 sudo apt install -y software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
+sudo apt-get install -y ca-certificates
 sudo apt install -y ansible
 sudo apt install -y openjdk-11-jdk
-sudo apt install -y jenkins
+wget -qO - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install -y jenkins
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 echo "Waiting for Jenkins to start up..."
